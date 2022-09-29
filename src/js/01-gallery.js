f548781a9fecd8285a2ce717279a15067db255ec
Anton <104930896+Zmiievskyi@users.noreply.github.com>
2022-09-29 22:19:55 +0100

    01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,39 +8,23 @@ const refs = document.querySelector('.gallery');
 
 const markupItem = galleryItems
   .map(
-    ({ preview, original, description }) => `<div class="gallery__item">
-  <a class="gallery__link" href=${original}>
-    <img
-      class="gallery__image"
-      src=${preview}
-      data-source=${original}
-      alt=${description}
-    />
-  </a>
-</div>`
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" 
+         href=${original}>
+            <img class="gallery__image" 
+            src=${preview} 
+            alt=${description} 
+            title=${description}/>
+        </a>`
   )
   .join('');
 
 refs.innerHTML = markupItem;
 
-const onClick = event => {
-  event.preventDefault();
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionData: 'alt',
+});
 
-  const { target } = event;
-  if (target.nodeName !== 'IMG') return;
 
-  const instance = basicLightbox.create(`
-<img src=${target.dataset.source}>`);
-
-  instance.show();
-
-  const onClose = e => {
-    if (e.code === 'Escape') instance.close();
-    window.removeEventListener('keydown', onClose);
-  };
-  window.addEventListener('keydown', onClose);
-};
-
-refs.addEventListener('click', onClick);
-
-console.log('sdfdf');
+console.dir(lightbox);
